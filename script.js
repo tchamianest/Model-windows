8265331ae707923531e6af6f3d43c7b04ecb1216
tchami <131756888+tchamianest@users.noreply.github.com>
2023-08-31 10:52:59 +0200

    Add files via upload
    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnclose = document.querySelector('.close-modal');
+const btncheck = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btncheck.length; i++)
+  btncheck[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+    document.querySelector('.title').innerHTML = `I'm a modal window ${
+      i + 1
+    } 😍`;
+  });
+
+const closev = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnclose.addEventListener('click', closev);
+overlay.addEventListener('click', closev);
